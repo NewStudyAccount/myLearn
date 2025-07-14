9b032dc1b3d3762f244cb8a88cc94447d5af49ba
乌鸦坐飞机 <mr.boss.qjj@gmail.com>
2025-07-14 23:08:56 +0800

    add

--- myproject/myproject-server/src/main/resources/db/myproject.sql
+++ myproject/myproject-server/src/main/resources/db/myproject.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80405
  File Encoding         : 65001
 
- Date: 08/07/2025 01:20:53
+ Date: 14/07/2025 23:08:05
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `sys_article`  (
   `is_deleted` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '删除标志',
   `read_num` int NULL DEFAULT 0 COMMENT '阅读次数',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '文章表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '文章表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_article
@@ -113,14 +113,17 @@ CREATE TABLE `sys_menu`  (
   `component` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '组件路径',
   `component_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL,
   PRIMARY KEY (`menu_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '菜单权限表' ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2002 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '菜单权限表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of sys_menu
 -- ----------------------------
-INSERT INTO `sys_menu` VALUES (1, '用户列表', 'test:001', 'C', 1, 1, '/admin/user', 'UserListView', 'User');
-INSERT INTO `sys_menu` VALUES (2, '角色列表', 'confirm:002', 'C', 2, 1, '/admin/role', 'RoleListView', 'Role');
-INSERT INTO `sys_menu` VALUES (3, '菜单列表', 'test:002', 'C', 3, 1, '/admin/menu', 'MenuListView', 'Menu');
+INSERT INTO `sys_menu` VALUES (1, '系统管理', 's', 'M', 1, 0, '/system', NULL, NULL);
+INSERT INTO `sys_menu` VALUES (2, '文章管理', 's', 'M', 2, 0, '/blog', NULL, NULL);
+INSERT INTO `sys_menu` VALUES (1001, '用户列表', 'test:001', 'C', 1, 1, '/system/user', 'system/user/User', 'User');
+INSERT INTO `sys_menu` VALUES (1002, '角色列表', 'confirm:002', 'C', 2, 1, '/system/role', 'system/role/Role', 'Role');
+INSERT INTO `sys_menu` VALUES (1003, '菜单列表', 'test:002', 'C', 3, 1, '/system/menu', 'system/menu/Menu', 'Menu');
+INSERT INTO `sys_menu` VALUES (2001, '标签列表', 's', 'C', 1, 2, '/blog/tag', 'blog/tag/Tags', NULL);
 
 -- ----------------------------
 -- Table structure for sys_role
@@ -195,7 +198,7 @@ CREATE TABLE `sys_user`  (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES (1, '张三', '$2a$10$AhCqTU/K8pI3ry5XmyF.FOUmKU8sOPeUWJs9/Rtl.zwDkBLdUvw9S', NULL, '1', NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_user` VALUES (1, 'zs', '$2a$10$AhCqTU/K8pI3ry5XmyF.FOUmKU8sOPeUWJs9/Rtl.zwDkBLdUvw9S', NULL, '1', NULL, NULL, NULL, NULL, NULL, '0');
 
 -- ----------------------------
 -- Table structure for sys_user_role
