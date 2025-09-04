bc907f7038dd81754d610bd926ac37073ac708fa
qianjj7 <qianjj7@asiainfo.com>
2025-09-04 20:53:49 +0800

    集合

---
+++ demo-learn/collection-learn/src/main/java/com/example/collectionlearn/CollectionLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.collectionlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionLearnApplication.class, args);
+    }
+
+}
