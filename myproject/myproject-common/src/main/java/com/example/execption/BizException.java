ac8261ebcf79487e3b4b2b6c76e3661847902e27
qianjj7 <qianjj7@asiainfo.com>
2026-04-01 17:14:38 +0800

    generator

--- myproject/myproject-common/src/main/java/com/example/execption/BizException.java
+++ myproject/myproject-common/src/main/java/com/example/execption/BizException.java
@@ -1,11 +1,11 @@
 package com.example.execption;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter
-@Setter
+@Data
 public class BizException extends RuntimeException {
     // 异常码
     private String errorCode;
