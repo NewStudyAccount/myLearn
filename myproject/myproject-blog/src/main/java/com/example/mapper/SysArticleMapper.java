131619790ac2280f06164865e166d69fdf26d44f
乌鸦坐飞机 <mr.boss.qjj@gmail.com>
2026-05-11 23:01:34 +0800

    remove

--- myproject/myproject-blog/src/main/java/com/example/mapper/SysArticleMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.domain.pojo.SysArticle;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysArticleMapper extends BaseMapper<SysArticle> {
-}
--- myproject/myproject-blog/src/main/java/com/example/mapper/SysCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.domain.pojo.SysCategory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysCategoryMapper extends BaseMapper<SysCategory> {
-}
